a692d1126dd7be5fdbaeea1339c29d4be9f869e1
Alexandre Beaulieu <abeaulieu815@gmail.com>
2025-03-16 10:48:29 -0300

    Removed Black border

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -57,7 +57,7 @@ export function Chat() {
         {isLoading && <ThinkingMessage />}
         <div ref={messagesEndRef} className="shrink-0 min-w-[24px] min-h-[24px]" />
       </div>
-      <div className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
+      <div className="flex mx-auto px-4 transparent pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
         <ChatInput  
           question={question}
           setQuestion={setQuestion}
